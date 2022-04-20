8f0ac9def985d3525a0e2b8c3f9aa9546a38e1e9
lxlongKunkka <110076790@qq.com>
2022-04-20 16:50:33 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -2,7 +2,8 @@
 Servo.SetLED(1, false)
 Servo.SetLED(2, true)
 Servo.SetLED(3, false)
-
+LCD1IN8.LCD_Init()
+LCD1IN8.LCD_Filling(COLOR.BLUE)
 basic.forever(function() {
     Servo.SetLED(1, false)
     Servo.SetLED(2, true)
@@ -12,4 +13,10 @@ basic.forever(function() {
     Servo.SetLED(2, false)
     Servo.SetLED(3, true)
     basic.pause(1000);
+})
+input.onButtonPressed(Button.A, function() {
+    LCD1IN8.DrawRectangle(randint(0, 80), randint(0, 80), randint(80, 160), randint(80, 160), randint(0, 65535), DRAW_FILL.DRAW_EMPTY, DOT_PIXEL.DOT_PIXEL_1)
+})
+input.onButtonPressed(Button.B, function() {
+    LCD1IN8.LCD_Clear()
 })
