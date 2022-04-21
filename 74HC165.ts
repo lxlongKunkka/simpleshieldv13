d17b15d61239323a4c37e45af9c2d078b0e53e95
lxlongKunkka <110076790@qq.com>
2022-04-21 17:07:07 +0800

    Update 74HC165.ts, test.ts

--- 74HC165.ts
+++ 74HC165.ts
@@ -19,74 +19,72 @@ enum KEY {
 
 //% weight=20 color=#3333ff icon="\uf11b"
 namespace SimpleShieldKey {
-    function Read74HC165(): number {
-        let val = 0;
-        Servo.FullOff(8);
-        Servo.SetLED(1, false);
-        control.waitMicros(21000);
-        Servo.FullOn(8);
+    let KEYSCAN = 0;
+    //% blockID==Listen_Key
+    //% block="Listen_Key"
+    //% weight=90
+    export function Read74HC165(): void {
         Servo.SetLED(1, true);
+        pins.setPull(INSR0_DATA, PinPullMode.PullUp)
+        pins.digitalWritePin(SR_CLK, 1);
+        Servo.FullOn(8);
+        control.waitMicros(5000);
+        Servo.FullOff(8);
+        control.waitMicros(20000);
+        Servo.FullOn(8);
+        pins.digitalWritePin(SR_CLK, 0);
+        control.waitMicros(5000);
+        KEYSCAN = 0;
         let i = 0;
-        for (i = 0; i < 16; i++) {
-            val = val << 1;
+        for (i = 0; i < 8; i++) {
+            KEYSCAN = KEYSCAN << 1;
             let tmp = pins.digitalReadPin(INSR0_DATA);
-            val |= tmp;
+            KEYSCAN |= tmp;
             pins.digitalWritePin(SR_CLK, 0);
             control.waitMicros(10000);
             pins.digitalWritePin(SR_CLK, 1);
-            control.waitMicros(2000);
+            control.waitMicros(5000);
         }
-
-        return val;
+        Servo.SetLED(1, false);
+        basic.showNumber(KEYSCAN);
     }
 
     //% blockID==Listen_Key
     //% block="Key %pin |Press"
     //% weight=90
     export function Listen_Key(pin: KEY): boolean {
-        let val = Read74HC165();
         let res = 1;
         switch (pin) {
             case KEY.UP:
-                res = (val >> 1 + 8) & 0x01;
+                res = (KEYSCAN >> 1) & 0x01;
                 break;
             case KEY.DOWN:
-                res = (val >> 2 + 8) & 0x01;
+                res = (KEYSCAN >> 2) & 0x01;
                 break;
             case KEY.LEFT:
-                res = (val >> 0 + 8) & 0x01;
+                res = (KEYSCAN >> 0) & 0x01;
                 break;
             case KEY.RIGHT:
-                res = (val >> 3 + 8) & 0x01;
+                res = (KEYSCAN >> 3) & 0x01;
                 break;
             case KEY.A:
-                res = (val >> 4 + 8) & 0x01;
+                res = (KEYSCAN >> 4) & 0x01;
                 break;
             case KEY.B:
-                res = (val >> 5 + 8) & 0x01;
+                res = (KEYSCAN >> 5) & 0x01;
                 break;
             case KEY.MENU:
-                res = (val >> 6 + 8) & 0x01;
-                break;
-            case KEY.IN1:
-                res = (val >> 3) & 0x01;
-                break;
-            case KEY.IN2:
-                res = (val >> 2) & 0x01;
-                break;
-            case KEY.IN3:
-                res = (val >> 1) & 0x01;
-                break;
-            case KEY.IN4:
-                res = (val >> 0) & 0x01;
+                res = (KEYSCAN >> 6) & 0x01;
                 break;
             default:
-                break;
+                return false;
         }
-        if (res == 1) {
+        if(res == 1)
+        {
             return false;
         }
-        else {
+        else
+        {
             return true;
         }
     }
--- test.ts
+++ test.ts
@@ -1,45 +1,24 @@
 // tests go here; this will not be compiled when this package is used as an extension.
 Servo.SetLED(2, true)
-Servo.SetLED(3, false)
 LCD1IN8.LCD_Init()
 LCD1IN8.LCD_Filling(COLOR.BLUE)
 music.playTone(Note.C, music.beat())
 basic.forever(function() {
     Servo.SetLED(2, true)
-    Servo.SetLED(3, false)
     basic.pause(1000);
     Servo.SetLED(2, false)
-    Servo.SetLED(3, true)
     basic.pause(1000);
 })
-
+let item: number = 1
 basic.forever(function() {
+    SimpleShieldKey.Read74HC165()
     if (SimpleShieldKey.Listen_Key(KEY.A)) {
-        LCD1IN8.DrawRectangle(randint(0, 80), randint(0, 80), randint(80, 160), randint(80, 160), randint(0, 65535), DRAW_FILL.DRAW_EMPTY, DOT_PIXEL.DOT_PIXEL_1);
+        Servo.SetLED(3, true)
     }
     if (SimpleShieldKey.Listen_Key(KEY.B)) {
-        LCD1IN8.LCD_Filling(randint(0, 65535))
+        Servo.SetLED(3, false)
     }
-    if (SimpleShieldKey.Listen_Key(KEY.UP)) {
-        //Motor.MotorRun(Motors.M1, 255)
-        Servo.Servo(12, 0)
-    }
-    if (SimpleShieldKey.Listen_Key(KEY.DOWN)) {
-        //Motor.MotorRun(Motors.M1, -255)
-        Servo.Servo(12, 60)
-    }
-    if (SimpleShieldKey.Listen_Key(KEY.LEFT)) {
-        //Motor.MotorRun(Motors.M1, 0)
-        Servo.Servo(12, 120)
-    }
-    if (SimpleShieldKey.Listen_Key(KEY.RIGHT)) {
-        //Motor.MotorRun(Motors.M1, 0)
-        Servo.Servo(12, 180)
-    }
-    if (input.buttonIsPressed(Button.A)) {
-        LCD1IN8.DrawRectangle(randint(0, 80), randint(0, 80), randint(80, 160), randint(80, 160), randint(0, 65535), DRAW_FILL.DRAW_EMPTY, DOT_PIXEL.DOT_PIXEL_1);
-    }
-    if (input.buttonIsPressed(Button.B)) {
-        LCD1IN8.LCD_Filling(randint(0, 65535))
+    if (SimpleShieldKey.Listen_Key(KEY.MENU)) {
+        Servo.SetLED(3, false)
     }
 })
